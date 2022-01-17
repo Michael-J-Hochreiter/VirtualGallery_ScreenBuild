e43bfeb22f1a6c38b7fa7d5645780cbdc32f55ab
Gerald <S2010238080@students.fh-hagenberg.at>
2022-01-17 23:50:30 +0100

    added teleportation hint finally

--- ScreenBuild/Assets/VirtualGallery/Scripts/TeleportationHint.cs
+++ ScreenBuild/Assets/VirtualGallery/Scripts/TeleportationHint.cs
@@ -4,38 +4,18 @@ using UnityEngine.EventSystems;
 
 namespace VirtualGallery.Scripts
 {
-    public class TeleportationHint : MonoBehaviour
+    public class TeleportationHint : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         public TextMeshProUGUI teleportationHintText;
-        
-        public void OnMouseEnter()
-        {
-            teleportationHintText.CrossFadeAlpha(1, 0.1f, false);
-            Debug.Log("enter");
-        }
-        
-        public void OnMouseOver()
-        {
-            teleportationHintText.CrossFadeAlpha(1, 0.1f, false);
-            Debug.Log("over");
-        }
 
-        public void OnMouseExit()
-        {
-            teleportationHintText.CrossFadeAlpha(0, 0.1f, false);
-            Debug.Log("exit");
-        }
-        
         public void OnPointerEnter(PointerEventData pointerEventData)
         {
-            //Output to console the GameObject's name and the following message
-            Debug.Log("Cursor Entering " + name + " GameObject");
+            teleportationHintText.CrossFadeAlpha(1, 0.2f, true);
         }
-        
+
         public void OnPointerExit(PointerEventData pointerEventData)
         {
-            //Output the following message with the GameObject's name
-            Debug.Log("Cursor Exiting " + name + " GameObject");
+            teleportationHintText.CrossFadeAlpha(0, 0.2f, true);
         }
     }
 }
